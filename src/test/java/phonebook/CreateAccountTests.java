367b45b28772077cea5c1ee7639f25b7bf96c8e8
Natalia Bodnaryuk <bodnata.o@gmail.com>
2023-09-05 09:32:49 +0200

    add LoginTests, RemoveContactTests,AddContactTests

--- src/test/java/phonebook/CreateAccountTests.java
+++ src/test/java/phonebook/CreateAccountTests.java
@@ -1,6 +1,5 @@
 package phonebook;
 
-import org.openqa.selenium.By;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -11,28 +10,18 @@ public class CreateAccountTests extends TestBase {
 
     @BeforeMethod
     public  void ensurePrecondition(){
-     if(!isElementPresent(By.cssSelector("a:nth-child(4)"))){
-         driver.findElement(By.xpath("button[contains(.,'Sign Out')]")).click();
-     }
-      driver.findElement(By.cssSelector("a:nth-child(4)")).click();
+     if(!isLoginLinkPresent()){
+         clickOnSignOutButton();
+       }
+        clickOnLoginLink();
     }
+
     @Test
-    public void newUserRegistrationPositiveTest(){
-        driver.findElement(By.cssSelector("[placeholder='Email']")).click();
-        driver.findElement(By.cssSelector("[placeholder='Email']")).clear();
-        driver.findElement(By.cssSelector("[placeholder='Email']")).sendKeys("nbo007@gmail.com");
+    public void existedUserRegistrationNegativeTest(){
+        fillLoginRegistrationForm("nbo007@gmail.com", "Olna123488$");
 
-        driver.findElement(By.cssSelector("[placeholder='Password'] ")).click();
-        driver.findElement(By.cssSelector("[placeholder='Password'] ")).clear();
-        driver.findElement(By.cssSelector("[placeholder='Password'] ")).sendKeys("Olna123488$");
+        clickOnRegistrationButton();
 
-       driver.findElement(By.xpath("//button[text()='Registration']")).click();
-       Assert.assertTrue(isElementPresent2(By.xpath("//button[contains(.,'Sign Out')]")));
+       Assert.assertTrue(isAlertPresent());
     }
-        // locator
-    // click on Login link   - a:nth-child(4) - css
-    // enter email    - [placeholder='Email'] - css
-    //enter password    -  [placeholder='Password'] - css
-    //click on Registration button    - //button[text()='Registration'] - xpath
-    //assert Sign out button displayed    - //button[contains(.,'Sign Out')] -xpath
 }
